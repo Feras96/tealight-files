21710de9f435437664b0725ed61f4a8757266883
Feras96 <ferasapps@gmail.com>
2014-08-19 12:22:58 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -3,9 +3,9 @@ from tealight.logo import move, turn
 for i in range(0,100):
   n = 10 + (i*10)
   while n < 80:
-  move(n)
-  turn(90)
-  move(10)
+    move(n)
+    turn(90)
+    move(10)
    
   
  
