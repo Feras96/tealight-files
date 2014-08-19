97152e8c016956802c0e37b6705abb10398352c2
Feras96 <ferasapps@gmail.com>
2014-08-19 15:18:09 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -12,7 +12,7 @@ while touch() != 'wall':
       turn(1)
     elif left_side() != 'wall': 
       turn(-1)
-    elif left_side() and right_side() == 'wall':
+    elif (left_side() and right_side()) == 'wall':
       turn(2)
       
    
