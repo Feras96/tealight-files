fc3d97e0de0886379fabaf325993e16b8217e4ba
Feras96 <ferasapps@gmail.com>
2014-08-20 10:18:00 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -3,11 +3,11 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 x = 0
 y = 150
 
-width = 20
-height = 8
+width = 8  
+height = 20
 
 for i in range(0,height):
-  for j in range(0,width):
+  for j in range(0,height):
     if i % 4 == 0:
       image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
     else:
