2fa00582a6ba1de8a9e1211022dd105649c9e4ff
Feras96 <ferasapps@gmail.com>
2014-08-20 10:50:04 +0100

    Running art/sketch.py

--- art/sketch.py
+++ art/sketch.py
@@ -3,10 +3,7 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 lastx = 0
 lasty = 0
 
-if button == "left"
-  color("blue")
-elif button == "right"
-  color("red")
+color("red")
 
 def handle_mousedown(x,y):
   global lastx, lasty
