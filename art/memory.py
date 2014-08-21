f9c355b1d521cc8ecbce260ea2ce4ed4b07db6a8
Feras96 <ferasapps@gmail.com>
2014-08-21 11:09:09 +0100

    Running art/memory.py

--- art/memory.py
+++ art/memory.py
@@ -15,6 +15,8 @@ y = 150
 width = 8   
 height = 6
 
+Cards = [i for i in range(2)]+[i for in in range(2)]
+
 for i in range(0,height):
   for j in range(0,width):
       image(x + i * 100, y + j * 100, "misc/Card.png")
