47adeaea2edb455fe333ad8095e12eeda76e498c
Feras96 <ferasapps@gmail.com>
2014-08-21 10:49:56 +0100

    Running art/memory.py

--- art/memory.py
+++ art/memory.py
@@ -19,6 +19,18 @@ for i in range(0,height):
   for j in range(0,width):
       image(x + i * 100, y + j * 100, "misc/Card.png")
       
+class Cards(object):
+  name = ""
+  rank = 0
+  
+  def __init__(self, name, rank):
+    self.name = name
+    self.rank = rank
+    
+def Show_cards(name, rank):
+  Card_shown = Cards(name , rank)
+  return Card_shown
+  
 text(screen_width/2 - 50, 100, "Memory Game!")
 
 
