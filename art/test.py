80ea13f7277e94f9a53cbab40867c46e503bb2bd
Feras96 <ferasapps@gmail.com>
2014-08-21 10:50:48 +0100

    Running art/test.py

--- art/test.py
+++ art/test.py
@@ -1,2 +1,13 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
+class Cards(object):
+  name = ""
+  rank = 0
+  
+  def __init__(self, name, rank):
+    self.name = name
+    self.rank = rank
+    
+def Show_cards(name, rank):
+  Card_shown = Cards(name , rank)
+  return Card_shown
