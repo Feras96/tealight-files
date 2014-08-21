fe46599a69100c9d1e3f484f5e43a115c9f570d6
Feras96 <ferasapps@gmail.com>
2014-08-21 10:24:45 +0100

    Running art/memory.py

--- art/memory.py
+++ art/memory.py
@@ -12,12 +12,13 @@ background("paper.jpg")
 x = 150
 y = 150
 
-width = 8   
-height = 6
+width = 6   
+height = 8
 
 for i in range(0,height):
   for j in range(0,width):
       image(x + i * 100, y + j * 100, "misc/Card.png")
+      
 text(screen_width/2 - 50, 100, "Memory Game!")
 
 
