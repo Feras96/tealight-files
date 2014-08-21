6a64f2f26ac2813d4af088c2adeaf0730a744551
Feras96 <ferasapps@gmail.com>
2014-08-21 11:10:57 +0100

    Closing art/test.py

--- art/test.py
+++ art/test.py
@@ -1,19 +1,20 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 
-name = ["a", "b", "c"]
-rank = [1, 2]
+#name = ["a", "b", "c"]
+#rank = [1, 2]
 
-class Cards(object):
-  name = ""
-  rank = 0
-  
-  def __init__(self, name, rank):
-    self.name = name
-    self.rank = rank
+#class Cards(object):
+#  name = ""
+#  rank = 0
+#  
+#  def __init__(self, name, rank):
+#    self.name = name
+#    self.rank = rank
     
-def Show_cards(name, rank):
-  Card_shown = Cards(name , rank)
-  return Card_shown
+#def Show_cards(name, rank):
+#  Card_shown = Cards(name , rank)
+#  return Card_shown
 
-print ("a", 2)
+#print ("a", 2)
+
